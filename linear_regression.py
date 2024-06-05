9eff49d9c92f0e367b8c7eaf6c4f5207876526a0
Randy Kipkurui <84719074+randy-kip@users.noreply.github.com>
2024-06-05 16:21:52 +0300

    Create linear_regression.py
    Solve the problems given. The problems are commented.

---
+++ linear_regression.py
@@ -0,0 +1,43 @@
+x = [1, 2, 3]
+y = [5, 1, 3]
+
+#y = x
+m1 = 1
+b1 = 0
+
+#y = 0.5x + 1
+m2 = 0.5
+b2 = 1
+
+# Find the y-values that the line with weights m1 and b1 would predict for the x-values given. Store these in a list called y_predicted1.
+# y = mx + b
+y_predicted1 = [m1 * xi + b1 for xi in x]
+
+# Find the y values that the line with weights m2 and b2 would predict for the x-values given. Store these in a list called y_predicted2.
+y_predicted2 = [m2 * xi + b2 for xi in x]
+
+print("Predicted y-values for line 1:", y_predicted1)
+print("Predicted y-values for line 2:", y_predicted2)
+
+#  find the sum of the squared distance between the actual y-values of the points and the y_predicted1 values
+total_loss1 = 0
+for yi, y_pred in zip(y, y_predicted1):
+  difference = yi - y_pred
+  squared_difference = difference ** 2
+  total_loss1 += squared_difference
+
+print("Total loss:", total_loss1)
+
+# sum of the squared distance between the actual y-values of the points and the y_predicted2 values
+total_loss2 = 0
+for i in range(len(y)):
+  total_loss2 += (y[i] - y_predicted2[i]) ** 2
+
+print("Total loss 2:", total_loss2)
+
+# Print out total_loss1 and total_loss2. Out of these two lines, which would you use to model the points
+# The line that produces the lowest total loss will be the line of better fit.
+if total_loss2 < total_loss1:
+  better_fit = 2
+else:
+  better_fit = 1
